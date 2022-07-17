07e73a97bf091c444913b42066fceab20668be09
FelipeBattistotti <felipe.battistotti@gmail.com>
2022-07-16 23:32:41 -0300

    Update LocaleNames

--- index.d.ts
+++ index.d.ts
@@ -70,13 +70,6 @@ type TDateRange = {
 type LocaleNames =
   | "af"
   | "en-au"
-  | "en-ca"
-  | "en-gb"
-  | "en-ie"
-  | "en-il"
-  | "en-in"
-  | "en-nz"
-  | "en-sg"
   | "fr"
   | "pt-br"
 
--- src/definitions.js
+++ src/definitions.js
@@ -1,13 +1,6 @@
 const localeNames = [
   "af",
   "en-au",
-  "en-ca",
-  "en-gb",
-  "en-ie",
-  "en-il",
-  "en-in",
-  "en-nz",
-  "en-sg",
   "fr",
   "pt-br"
 ]
