e4e3da392dbfb6035fea11190b4b071d238098f4
FelipeBattistotti <felipe.battistotti@gmail.com>
2022-07-17 00:16:48 -0300

    Update function returnsLocale

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,34 @@
-const returnsLocale = (str = '') => {
-  
+const af = require("../locales/af")
+const enAu = require("../locales/enAu")
+const fr = require("../locales/fr")
+const ptBr = require("../locales/ptBr")
+
+/**
+ * 
+ * @param localeName string
+ * @returns locale object
+ */
+const returnsLocale = (localeName = '') => {
+  let locale = null
+
+  switch (localeName) {
+    case "af":
+      locale = af
+      break
+    case "en-au":
+      locale = enAu
+      break
+    case "fr":
+      locale = fr
+      break
+    case "pt-br":
+      locale = ptBr
+      break
+    default:
+      locale = ptBr
+  }
+
+  return locale
 }
 
 module.exports = { returnsLocale }
